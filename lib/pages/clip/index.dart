f67af31a4473a9e8da2b0ab7dbde1b14c175630c
zhuchuanyong <zhuchuanyong@vtstar.net>
2019-11-22 22:39:13 +0800

    fea:圆角

--- lib/pages/clip/index.dart
+++ lib/pages/clip/index.dart
@@ -11,7 +11,50 @@ class ClipDemoPage extends StatelessWidget {
       body: new Container(
         alignment: Alignment.center,
         margin: EdgeInsets.all(10),
-        child: Text('hahha'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("BoxDecoration 圆角"),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/meizi.jpg'),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            new Text("BoxDecoration 圆角对 child"),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              child: Image.asset(
+                'assets/images/meizi.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            new Text("ClipRRect 圆角对 child"),
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              child: Image.asset(
+                'assets/images/meizi.jpg',
+                fit: BoxFit.cover,
+                width: 100,
+                height: 100,
+              ),
+            )
+          ],
+        ),
         // child: new Column(
         //   crossAxisAlignment: CrossAxisAlignment.center,
         //   mainAxisAlignment: MainAxisAlignment.center,
