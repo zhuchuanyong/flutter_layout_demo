d622e80f13919e1c04d6b1ef537e6874c90a2cb8
zhuchuanyong <zhu1251637837@gmail.com>
2020-11-28 20:56:37 +0800

    gridView

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -6,7 +6,74 @@ class ViewDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageViewBuilderDemo();
+    return GridViewBuilderDemo();
+  }
+}
+
+class GridViewBuilderDemo extends StatelessWidget {
+  const GridViewBuilderDemo({Key key}) : super(key: key);
+  Widget _gridItemBuilder(BuildContext context, int index) {
+    return Container(
+        child: Image.network(posts[index].imageUrl, fit: BoxFit.cover));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, crossAxisSpacing: 8.0, mainAxisSpacing: 8.0),
+        itemBuilder: _gridItemBuilder,
+        itemCount: posts.length);
+  }
+}
+
+class GridViewExtentDemo extends StatelessWidget {
+  const GridViewExtentDemo({Key key}) : super(key: key);
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text(
+          "Item $index",
+          style: TextStyle(fontSize: 18.0, color: Colors.grey),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+        mainAxisSpacing: 16.0,
+        maxCrossAxisExtent: 150.0,
+        crossAxisSpacing: 16.0,
+        children: _buildTiles(100));
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
+  const GridViewCountDemo({Key key}) : super(key: key);
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text(
+          "Item",
+          style: TextStyle(fontSize: 18.0, color: Colors.grey),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 16.0,
+        crossAxisSpacing: 16.0,
+        children: _buildTiles(100));
   }
 }
 
